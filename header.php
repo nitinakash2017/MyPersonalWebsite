c0a6d946de434782a0cd5e65f0583e89eeccc91c
Nitin Akash <nitinakash@Nitins-MacBook-Air.local>
2017-12-25 18:13:29 -0400

    Adding basic bootstrap template, Arranging css and js directory and dividing the layout into header, footer and containt

--- header.php
+++ header.php
@@ -1,9 +1,21 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: nitinakash
- * Date: 2017-12-25
- * Time: 4:04 PM
- */
-?>
-<h1>Nitin Akash</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Cover Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="css/cover.css" rel="stylesheet">
+</head>
+
+<body>
+
+
