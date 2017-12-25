960382e736d1902867add5d05820a4298e028e59
Nitin Akash <nitinakash@Nitins-MacBook-Air.local>
2017-12-25 18:19:37 -0400

    Rearrange Header, Footer and Body

--- footer.php
+++ footer.php
@@ -1,3 +1,14 @@
+<div class="mastfoot">
+    <div class="inner">
+        <p>Cover template for <a href="https://getbootstrap.com">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+    </div>
+</div>
+
+</div>
+
+</div>
+
+</div>
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
--- header.php
+++ header.php
@@ -17,5 +17,19 @@
 </head>
 
 <body>
+    <div class="site-wrapper">
 
+        <div class="site-wrapper-inner">
 
+            <div class="cover-container">
+
+                <div class="masthead clearfix">
+                    <div class="inner">
+                        <h3 class="masthead-brand">Cover</h3>
+                        <nav class="nav nav-masthead">
+                            <a class="nav-link active" href="#">Home</a>
+                            <a class="nav-link" href="#">Features</a>
+                            <a class="nav-link" href="#">Contact</a>
+                        </nav>
+                    </div>
+                </div>
