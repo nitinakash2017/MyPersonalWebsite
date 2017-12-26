a7a90fcb5e7d587abbd5d3e96d6e90deb333b515
Nitin Akash <nitinakash@Nitins-MacBook-Air.local>
2017-12-26 12:43:16 -0400

    Made body section of index page

--- index.php
+++ index.php
@@ -1,16 +1,26 @@
 <?php include 'header.php';
 ?>
-
+    <div class="container">
             <div class="inner cover">
+
+                    <img src="image//Nitin.jpg" style="border-radius: 50%" width="150px" height="150px"/>
                 <h1 class="cover-heading">Graduate Student at Saint Mary's University</h1>
-                <h4>Halifax Nova Scotia, Cananda</h4>
-                <h4>Me talking about myself</h4>
-                <p class="lead">I am currently enrolled in Masters in Computing and Data Analytics course.  I have total of 3 years and 7 month of experience</p>
+                <p>(Tentative Graduation Date: 31-Dec-2018)</p>
+                <p>Halifax, Nova Scotia, Cananda</p>
+                <span style="color: #2aabd2" class="styleTalkingSpan"><u style="text-decoration-color: #2aabd2">Me talking about myself</u></span>
+                <ul style="text-align: left">
+                    <li>Course: Masters in Computing and Data Analytics</li>
+                    <li>Total Experience: 3 Years 7 month</li>
+                    <li>Domain: Finance</li>
+                    <li>Database Technology: Oracle, Plsql, Mysql, Sql Server(SSMS, SSIS)</li>
+                    <li>Web Technology: Java, Asp.Net Mvc, Php, Html, Javascript, Css</li>
+                    <li>Ongoing learning: Android, Ios, React, Php, Data Mining, Tableau</li>
+                </ul>
                 <p class="lead">
                     <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
                 </p>
             </div>
-
+    </div>
 
 <?php include 'footer.php';
 ?>
